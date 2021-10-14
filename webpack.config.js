d21c237f92bb0aa8e92ba133f19f1a7e470e17a3
Aiden <rheech22@gmail.com>
2021-10-14 18:28:27 +0900

    Try to set CI/CD

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: path.resolve(__dirname, 'src/index.jsx'),
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle-[hash].js',
+  },
   module: {
     rules: [
       {
@@ -14,4 +19,13 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx'],
   },
+  devServer: {
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'index.html'),
+      inject: 'body',
+    }),
+  ],
 };
