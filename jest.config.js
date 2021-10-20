1ef520c0e57ae1deacd3558606f8696f1986d4de
Aiden <rheech22@gmail.com>
2021-10-20 18:05:48 +0900

    Refactor test for redux slice

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  setupFilesAfterEnv: [
+    'given2/setup',
+    'jest-plugin-context/setup',
+    './jest.setup',
+  ],
   testEnvironment: 'jest-environment-jsdom',
-  setupFilesAfterEnv: ['./jest.setup.js'],
 };
