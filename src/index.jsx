003cef9027225bb0100258214b6017f1d933980b
Aiden <rheech22@gmail.com>
2021-10-24 05:10:03 +0900

    Apply base url

--- src/index.jsx
+++ src/index.jsx
@@ -11,7 +11,7 @@ import store from './store';
 ReactDOM.render(
   (
     <Provider store={store}>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <BrowserRouter basename="/project-react-5-rheech22">
         <App />
       </BrowserRouter>
     </Provider>
