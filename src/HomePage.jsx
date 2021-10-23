e46ddb0a4ac11c85ed9315e80f4cf50075af8f83
Aiden <rheech22@gmail.com>
2021-10-24 04:24:55 +0900

    Add router to separate pages
    식단 페이지를 별도로 구성하기 위해 라우터를 적용했습니다.

---
+++ src/HomePage.jsx
@@ -0,0 +1,63 @@
+import styled from '@emotion/styled';
+
+import {
+  useCallback,
+  useEffect,
+  useRef,
+} from 'react';
+
+import { useSelector } from 'react-redux';
+
+import CalculatorContainer from './CalculatorContainer';
+import ResultSheet from './ResultSheet';
+
+import { get } from './utils';
+
+const Container = styled.div({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  paddingTop: '1em',
+  '& h1, h2, h3': {
+    padding: '0',
+    margin: '0',
+  },
+  '& h1': {
+    fontSize: '2em',
+    fontWeight: '300',
+  },
+  '& h2': {
+    fontSize: '1.6em',
+    fontWeight: '300',
+  },
+  '& h3': {
+    fontSize: '1.3em',
+    fontWeight: '400',
+  },
+});
+
+export default function App() {
+  const { result } = useSelector(get('calories'));
+
+  const myRef = useRef(null);
+
+  const scrollToBottom = useCallback(() => {
+    if (result) {
+      myRef.current.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
+    }
+  }, [result]);
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [scrollToBottom]);
+
+  return (
+    <Container ref={myRef}>
+      <h1>My Cal</h1>
+      <p>당신의 칼로리를 확인하세요</p>
+      <CalculatorContainer />
+      <ResultSheet />
+    </Container>
+  );
+}
---
+++ src/HomePage.test.jsx
@@ -0,0 +1,70 @@
+import { render } from '@testing-library/react';
+
+import { useDispatch, useSelector } from 'react-redux';
+
+import HomePage from './HomePage';
+
+import { getActivityDescription } from './utils';
+
+jest.mock('react-redux');
+
+describe('HomePage', () => {
+  window.HTMLElement.prototype.scrollIntoView = jest.fn();
+
+  const dispatch = jest.fn();
+
+  beforeEach(() => {
+    dispatch.mockClear();
+
+    useDispatch.mockImplementation(() => dispatch);
+
+    useSelector.mockImplementation((selector) => selector({
+      bodyStats: {
+        gender: '',
+        age: 0,
+        height: 0,
+        weight: 0,
+        activity: {
+          level: 1,
+          description: getActivityDescription(1),
+        },
+        goalNubmer: 0,
+      },
+      calories: {
+        bmr: 1000,
+        tdee: 1500,
+        result: 2000,
+      },
+    }));
+  });
+
+  it('renders page title', () => {
+    const { container } = render((
+      <HomePage />
+    ));
+    expect(container).toHaveTextContent('My Cal');
+  });
+
+  it('renders inputs', () => {
+    const { queryByLabelText } = render((
+      <HomePage />
+    ));
+
+    expect(queryByLabelText('남')).not.toBeNull();
+    expect(queryByLabelText('여')).not.toBeNull();
+    expect(queryByLabelText('만 나이')).not.toBeNull();
+    expect(queryByLabelText('키(cm)')).not.toBeNull();
+    expect(queryByLabelText('몸무게(kg)')).not.toBeNull();
+    expect(queryByLabelText('Activity Level')).not.toBeNull();
+  });
+
+  it('renders calories', () => {
+    const { container } = render((
+      <HomePage />
+    ));
+
+    expect(container).toHaveTextContent('1000');
+    expect(container).toHaveTextContent('1500');
+    expect(container).toHaveTextContent('2000');
+  });
+});
